e2e63d5ec9c01d201b91d92f5671d3a599b372ce
BenThere <benjaminbirdsall@icloud.com>
2023-10-18 13:55:39 -0600

    fix spacing

--- server.js
+++ server.js
@@ -9,35 +9,35 @@ app.use(bodyParser.json());
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).json({ error: 'Something went wrong' });
-  });
+});
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
 
 app.post('/sms-replies', (req, res) => {
-  const textId = req.body.textId;
-  const fromNumber = req.body.fromNumber;
-  const text = req.body.text;
+    const textId = req.body.textId;
+    const fromNumber = req.body.fromNumber;
+    const text = req.body.text;
 
-  // Process the SMS reply here
-  console.log(`Received SMS reply from ${fromNumber}: ${text}`);
+    // Process the SMS reply here
+    console.log(`Received SMS reply from ${fromNumber}: ${text}`);
 
-  // You can send a response depending on the received reply
-  // ...
+    // You can send a response depending on the received reply
+    // ...
 
-  res.sendStatus(200); // Respond with a 200 OK status
+    res.sendStatus(200); // Respond with a 200 OK status
 });
 
 // Add the route for handling SMS replies
 app.post('/api/handleSmsReply', (req, res) => {
-  console.log(req.body)
+    console.log(req.body)
 
-  res.sendStatus(200); // Respond with a 200 OK status
+    res.sendStatus(200); // Respond with a 200 OK status
 });
 
 // Start the server
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
