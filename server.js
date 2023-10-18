39b8c308c8428e7835c231a6da486b060d90d3e0
BenThere <benjaminbirdsall@icloud.com>
2023-10-18 14:21:52 -0600

    remove unnecessary post route

--- server.js
+++ server.js
@@ -16,23 +16,16 @@ app.get('/', (req,res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
 
-app.post('/sms-replies', (req, res) => {
+app.post('/handleSmsReply', (req, res) => {
     const textId = req.body.textId;
     const fromNumber = req.body.fromNumber;
     const text = req.body.text;
 
     // Process the SMS reply here
-    console.log(`Received SMS reply from ${fromNumber}: ${text}`);
+    smsUtility.sendSMS(`Received SMS reply from ${fromNumber}: ${text}`);
 
     // You can send a response depending on the received reply
     // ...
-
-    res.sendStatus(200); // Respond with a 200 OK status
-});
-
-// Add the route for handling SMS replies
-app.post('/api/handleSmsReply', (req, res) => {
-    smsUtility.sendSMS(req.body)
 
     res.sendStatus(200); // Respond with a 200 OK status
 });
