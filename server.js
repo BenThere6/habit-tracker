7b2e399c41c967800f486b0b208b71f4466366ff
BenThere <benjaminbirdsall@icloud.com>
2023-10-19 14:14:33 -0600

    relocate routes

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
-const { sendSMS } = require('./utils/sms');
+const { sendSMS } = require('./utils/sendSms');
 const bodyParser = require('body-parser');
+const controllers = require('./controllers')
 
 const app = express();
 
@@ -12,19 +13,7 @@ app.use((err, req, res, next) => {
     res.status(500).json({ error: 'Something went wrong' });
 });
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/public/index.html');
-})
-
-app.post('/api/handleSmsReply', (req, res) => {
-    const textId = req.body.textId;
-    const fromNumber = req.body.fromNumber;
-    const text = req.body.text;
-
-    console.log(`Received SMS reply from ${fromNumber}: ${text}`);
-
-    res.sendStatus(200);
-});
+app.use(controllers);
 
 port = process.env.PORT || 3000;
 app.listen(port, () => {
