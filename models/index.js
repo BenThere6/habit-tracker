d60fe899ea6b4c92b896546dc693228428f53731
BenThere <benjaminbirdsall@icloud.com>
2023-10-19 17:51:33 -0600

    started table relations

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+const dbConfig = require('../config/sequelize');
+
+const sequelize = new Sequelize(dbConfig);
+
+const User = require('./user');
+const Habit = require('./habit');
+
+User.hasMany(Habit, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+Habit.belongsTo(User)
+
+module.exports = { sequelize, User, Habit };
