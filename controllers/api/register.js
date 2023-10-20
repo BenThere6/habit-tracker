f5d997cbc19e90eaa9b391aca459f6f1e1d57167
BenThere <benjaminbirdsall@icloud.com>
2023-10-19 21:22:42 -0600

    remove passport import

--- controllers/api/register.js
+++ controllers/api/register.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require('../../models');
-const passport = require('passport');
 
 // GET request to display the registration form
 router.get('/register', (req, res) => {
