ddca389506f74df638d574c1a7caac39bb4e3440
BenThere <benjaminbirdsall@icloud.com>
2023-10-19 21:52:06 -0600

    fix path

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const smsRoutes = require('./sms');
 const dashboardRoute = require('./dashboard');
-const { ensureAuthenticated } = require('./controllers/authMiddleware');
+const { ensureAuthenticated } = require('../authMiddleware');
 
 router.use('/dashboard', ensureAuthenticated, dashboardRoute);
 router.use('/sms', smsRoutes);
