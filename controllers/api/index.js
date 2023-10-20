4908ea6ed5ee9d379c7b0b4b0c42de366a3a1596
BenThere <benjaminbirdsall@icloud.com>
2023-10-19 22:19:01 -0600

    include login and register routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
 const smsRoutes = require('./sms');
 const dashboardRoute = require('./dashboard');
-const { ensureAuthenticated } = require('./auth/authMiddleware');
+const loginRoute = require('./auth/login');
+const registerRoute = require('./auth/register');
+const { ensureAuthenticated } = require('./auth/authMiddleware'); 
 
 router.use('/dashboard', ensureAuthenticated, dashboardRoute);
 router.use('/sms', smsRoutes);
+
+router.use('/login', loginRoute);
+router.use('/register', registerRoute);
 
 module.exports = router;
