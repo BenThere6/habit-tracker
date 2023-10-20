94b9a62233cce257db035e9715bdb50408a6014d
BenThere <benjaminbirdsall@icloud.com>
2023-10-19 21:31:49 -0600

    add dashboard route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const smsRoutes = require('./sms');
+const dashboardRoutes = require('./dashboard');
 
 router.use('/sms', smsRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
