374086b6b04f52013b16fa005fab4e6d47e13a79
BenThere <benjaminbirdsall@icloud.com>
2023-10-19 21:49:50 -0600

    add authentication check

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 const smsRoutes = require('./sms');
 const dashboardRoutes = require('./dashboard');
 
+const { ensureAuthenticated } = require('./controllers/authMiddleware');
+const dashboardRoute = require('./controllers/api/dashboard');
+
+router.use('/dashboard', ensureAuthenticated, dashboardRoute);
 router.use('/sms', smsRoutes);
-router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
