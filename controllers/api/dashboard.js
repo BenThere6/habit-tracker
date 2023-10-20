787b2295fd19f4910927bc7bead81e5400f4c573
BenThere <benjaminbirdsall@icloud.com>
2023-10-19 21:42:47 -0600

    fix path

--- controllers/api/dashboard.js
+++ controllers/api/dashboard.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     // Render your dashboard page here.
-    res.render('dashboard'); // Customize this to match your dashboard view.
+    res.render('../../views/dashboard'); // Customize this to match your dashboard view.
 });
 
 module.exports = router;
