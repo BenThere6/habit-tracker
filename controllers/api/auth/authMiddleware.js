a938eec76e26a443396ae31b2e2efd6c78bf8828
BenThere <benjaminbirdsall@icloud.com>
2023-10-19 22:13:40 -0600

    removed comment

--- controllers/api/auth/authMiddleware.js
+++ controllers/api/auth/authMiddleware.js
@@ -1,4 +1,3 @@
-// controllers/authMiddleware.js
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next(); // User is authenticated, proceed to the next middleware or route.
