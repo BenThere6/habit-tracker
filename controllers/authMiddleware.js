dd0e28474b3f543ab563ddc0e331a785a7c92942
BenThere <benjaminbirdsall@icloud.com>
2023-10-19 21:46:17 -0600

    add auth middleware

---
+++ controllers/authMiddleware.js
@@ -0,0 +1,10 @@
+// controllers/authMiddleware.js
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next(); // User is authenticated, proceed to the next middleware or route.
+    }
+    // User is not authenticated, redirect to the login page or send an error response.
+    res.redirect('/login'); // You can customize this to match your login route.
+}
+
+module.exports = { ensureAuthenticated };
