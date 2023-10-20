e8eb8827aa680ed276157ccd9a5a8d288dfcca71
BenThere <benjaminbirdsall@icloud.com>
2023-10-19 18:21:46 -0600

    add passport authentication

--- server.js
+++ server.js
@@ -1,19 +1,51 @@
 const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const flash = require('connect-flash');
 const morgan = require('morgan');
 const { sendSMS } = require('./utils/sendSms');
 const bodyParser = require('body-parser');
-const controllers = require('./controllers')
+const controllers = require('./controllers');
+const { User } = require('./models'); // Import your User model
 
 const app = express();
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ error: 'Something went wrong' });
-});
+app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true })); // Initialize session
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.use(controllers);
+
+// Configure the local strategy for passport
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+}, (email, password, done) => {
+    User.findOne({ where: { email } }).then(user => {
+        if (!user) {
+            return done(null, false, { message: 'Incorrect email.' });
+        }
+        if (!user.checkPassword(password)) {
+            return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+    });
+}));
+
+// Serialize and deserialize user data
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findByPk(id).then(user => {
+        done(null, user);
+    });
+});
 
 port = process.env.PORT || 3000;
 app.listen(port, () => {
