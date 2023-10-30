26fb97b6b9cf9cd7af30c28cbd832c1b85e00ac8
BenThere <benjaminbirdsall@icloud.com>
2023-10-30 16:42:21 -0600

    add 404 view

--- controllers/index.js
+++ controllers/index.js
@@ -6,5 +6,6 @@ const homeRoutes = require('./homeRoutes');
 router.use('/api', apiRouter);
 router.use('/auth', authRouter);
 router.use('/', homeRoutes);
+router.use((req,res) => {res.render('page-not-found')})
 
 module.exports = router;
