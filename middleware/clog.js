57a1baed3504ead0e8e09be02f1a5bae1fcb82d6
BenThere <benjaminbirdsall@icloud.com>
2024-01-04 12:35:42 -0700

    update clog middleware

--- middleware/clog.js
+++ middleware/clog.js
@@ -1,20 +1,34 @@
 const clog = (req, res, next) => {
-    const fgCyan = '\x1b[36m';
-    const fgWhite = '\x1b[37m';
-    
-    switch (req.method) {
-      case 'GET': {
-        console.info(`📗 ${res.statusCode} ${fgCyan}${req.method} request to ${req.path}${fgWhite}`);
-        break;
-      }
-      case 'POST': {
-        console.info(`📘 ${res.statusCode} ${fgCyan}${req.method} request to ${req.path}${fgWhite}`);
-        break;
-      }
-      default:
-        console.log(`📙 ${res.statusCode} ${fgCyan}${req.method} request to ${req.path}${fgWhite}`);
-    }
-    next();
+  const originalSend = res.send;
+  res.send = function(data) {
+      console.info(getLogString(req, res, data));
+      originalSend.apply(res, arguments);
   };
-  
-  exports.clog = clog;
+  next();
+};
+
+function getLogString(req, res, data) {
+  const fgCyan = '\x1b[36m';
+  const fgWhite = '\x1b[37m';
+  const method = req.method;
+  const path = req.path;
+  const statusCode = res.statusCode;
+  const statusEmoji = getEmojiByStatusCode(statusCode);
+
+  return `${statusEmoji} ${statusCode} ${fgCyan}${method} request to ${path}${fgWhite}`;
+}
+
+function getEmojiByStatusCode(statusCode) {
+  if (statusCode >= 200 && statusCode < 300) {
+      return '📗'; // Successful responses
+  } else if (statusCode >= 300 && statusCode < 400) {
+      return '📘'; // Redirection messages
+  } else if (statusCode >= 400 && statusCode < 500) {
+      return '📙'; // Client error responses
+  } else if (statusCode >= 500) {
+      return '📕'; // Server error responses
+  }
+  return '📓'; // Unknown status
+}
+
+exports.clog = clog;
