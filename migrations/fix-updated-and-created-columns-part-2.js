748b3aff8c3bb148b0ddc8c5ba48f4338a432686
BenThere <benjaminbirdsall@icloud.com>
2024-01-12 12:42:32 -0700

    remove old column

--- migrations/fix-updated-and-created-columns-part-2.js
+++ migrations/fix-updated-and-created-columns-part-2.js
@@ -1,19 +1,19 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  down: async (queryInterface, Sequelize) => {
     // Check if the column already exists before adding it
     const tableInfo = await queryInterface.describeTable('habit');
 
     if (!tableInfo.created_at) {
-      await queryInterface.addColumn('habit', 'created_at', {
+      await queryInterface.addColumn('habit', 'createdAt', {
         type: Sequelize.DATE,
         allowNull: false,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       });
     }
     if (!tableInfo.updated_at) {
-      await queryInterface.addColumn('habit', 'updated_at', {
+      await queryInterface.addColumn('habit', 'updatedAt', {
         type: Sequelize.DATE,
         allowNull: false,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
@@ -21,8 +21,8 @@ module.exports = {
     }
   },
 
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('habit', 'created_at');
-    await queryInterface.removeColumn('habit', 'updated_at');
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('habit', 'createdAt');
+    await queryInterface.removeColumn('habit', 'updatedAt');
   }
 };
